6dc0d129335dcbd56b0a9bd08809dfb516a17d70
mingqian.gui <mingqian.gui@boomingtech.com>
2025-06-11 14:31:35 +0800

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+Demo about binding C++ code to quickjs and call C++ function/class in js
+
+NOTE: **this project use [quickjs-ng](https://github.com/quickjs-ng/quickjs) rather than [bellard's quickjs](https://github.com/bellard/quickjs)  to make compile corss platform**
+
+## How To Build
+
+```bash
+cmake -S . -B cmake-build
+cmake --build cmake-build
+```
---
+++ demos/01-HelloWorld/main.cpp
@@ -0,0 +1,59 @@
+#include "quickjs.h"
+#include "quickjs-libc.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main() {
+    // create quickjs runtime
+    JSRuntime* runtime = JS_NewRuntime();
+    if (!runtime) {
+        std::cerr << "init runtime failed" << std::endl;
+        return 1;
+    }
+
+    // create context
+    JSContext* ctx = JS_NewContext(runtime);
+    if (!ctx) {
+        std::cerr << "create context failed" << std::endl;
+        JS_FreeRuntime(runtime);
+        return 2;
+    }
+
+    // [optional] init quickjs internal help functions(print, console.log)
+    // from quickjs-libc.h
+    js_std_add_helpers(ctx, 0, NULL);
+
+    // read js source code
+    std::ifstream file("demos/01-HelloWorld/main.js",
+                       std::ios::in | std::ios::binary);
+    if (!file) {
+        std::cerr << "open file main.js failed" << std::endl;
+    }
+    std::stringstream ss;
+    ss << file.rdbuf();
+    std::string content = ss.str();
+
+    // run script
+    JSValue result = JS_Eval(ctx, content.c_str(), content.size(), nullptr,
+                             JS_EVAL_FLAG_STRICT);
+
+    // exception handle
+    if (JS_IsException(result)) {
+        JSValue exception = JS_GetException(ctx);
+        std::cerr << "JS Error: " << JS_ToCString(ctx, exception) << std::endl;
+        JSValue stack = JS_GetPropertyStr(ctx, exception, "stack");
+        // if has stack info, output stack info
+        if (!JS_IsUndefined(stack)) {
+            std::cerr << "stack: " << JS_ToCString(ctx, stack) << std::endl;
+        }
+        JS_FreeValue(ctx, exception);
+        JS_FreeValue(ctx, stack);
+    }
+
+    // don't forget cleanup
+    JS_FreeValue(ctx, result);
+    JS_FreeContext(ctx);
+    JS_FreeRuntime(runtime);
+    return 0;
+}
